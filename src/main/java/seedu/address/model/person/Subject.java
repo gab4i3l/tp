bc2cd0e41b0071b51de5e88c3eafffb07a79897d
Daren Tan <daren28@gmail.com>
2026-03-10 20:28:51 +0800

    Fix: Checkstyle

--- src/main/java/seedu/address/model/person/Subject.java
+++ src/main/java/seedu/address/model/person/Subject.java
@@ -10,7 +10,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Subject {
 
-    public static final String MESSAGE_CONSTRAINTS = "Subject should only contain alphabetical characters and spaces, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS =
+            "Subject should only contain alphabetical characters and spaces, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
